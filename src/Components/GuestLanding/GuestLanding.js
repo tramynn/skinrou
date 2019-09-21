64d1278c1919c079b7ac4eb4b2d6ca7118406fec
Tramy Nguyen <tramynn2@gmail.com>
2019-09-21 18:58:50 -0500

    set up guest landing component

---
+++ src/Components/GuestLanding/GuestLanding.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../../styles/partials/GuestLanding/GuestLanding.css";
+
+export default class GuestLanding extends Component {
+  render() {
+    return (
+      <div className="Body">
+        <h1>SKINROU</h1>
+      </div>
+    );
+  }
+}
