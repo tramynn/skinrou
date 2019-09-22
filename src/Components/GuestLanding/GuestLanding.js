3b106268c34bc871341004b4dda9b75ea3763278
Tramy Nguyen <tramynn2@gmail.com>
2019-09-21 23:15:19 -0500

    made guest landing responsive

--- src/Components/GuestLanding/GuestLanding.js
+++ src/Components/GuestLanding/GuestLanding.js
@@ -5,12 +5,21 @@ import "../../styles/partials/GuestLanding/GuestLanding.css";
 export default class GuestLanding extends Component {
   render() {
     return (
-      <div className="Body">
+      <div className="GuestLanding-Body">
         <img src={headerLogo} width={320} alt="SKINROU" />
+        <p className="tagline">
+          Beauty starts with a routine;
+          <br />
+          treat your skin with priority.
+        </p>
         <div className="GuestLanding-Buttons">
-          <button>Login</button>
+          <div className="GL-buttons-container">
+            <button>Log In</button>
+          </div>
           <p>or</p>
-          <button>Register</button>
+          <div className="GL-buttons-container">
+            <button>Register</button>
+          </div>
         </div>
       </div>
     );
