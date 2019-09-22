782fc3d931b3676b0da8fd9f7e619ba2efeb84dd
Tramy Nguyen <tramynn2@gmail.com>
2019-09-21 21:52:26 -0500

    styled login and register buttons

--- src/Components/GuestLanding/GuestLanding.js
+++ src/Components/GuestLanding/GuestLanding.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import headerLogo from "../../images/main-logo.svg";
 import "../../styles/partials/GuestLanding/GuestLanding.css";
 
 export default class GuestLanding extends Component {
   render() {
     return (
       <div className="Body">
-        <h1>SKINROU</h1>
+        <img src={headerLogo} width={320} alt="SKINROU" />
+        <div className="GuestLanding-Buttons">
+          <button>Login</button>
+          <p>or</p>
+          <button>Register</button>
+        </div>
       </div>
     );
   }
