f7bf4798150170d518dbdfd9250d68152fb7ba55
Tramy Nguyen <tramynn2@gmail.com>
2019-10-06 16:13:02 -0500

    set up side menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,13 +9,21 @@ import {
   faSignOutAlt,
   faCog,
   faGripLinesVertical,
-  faCommentDots
+  faCommentDots,
+  faBars
 } from "@fortawesome/free-solid-svg-icons";
 import { getSession, logoutUser } from "../../redux/reducers/userReducer";
 import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menuOpenStatus: "side-menu"
+    };
+  }
+
   componentDidMount() {
     this.props.getSession();
   }
@@ -23,6 +31,17 @@ class Header extends Component {
   handleLogout = () => {
     this.props.logoutUser();
     this.props.history.push("/");
+  };
+
+  toggleMenu = () => {
+    if (
+      this.state.menuOpenStatus === "side-menu-close" ||
+      this.state.menuOpenStatus === "side-menu"
+    ) {
+      this.setState({ menuOpenStatus: "side-menu-open" });
+    } else if (this.state.menuOpenStatus === "side-menu-close") {
+      this.setState({ menuOpenStatus: "side-menu-close" });
+    }
   };
 
   render() {
@@ -106,29 +125,89 @@ class Header extends Component {
               </Link>
             </ul>
           </nav>
+          <span className="Menu-bar hidden-by-default">
+            <FontAwesomeIcon
+              icon={faBars}
+              color="#FFFFFF"
+              size="2x"
+              className="Hamburger"
+            />
+          </span>
         </div>
-        {/* <nav className="Nav-collapsed-container">
+        <nav className="Nav-collapsed-container hidden-by-default">
           <ul className="Nav-collapsed-links">
-            <li>
-              <FontAwesomeIcon icon={faHome} /> Home
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faPlusCircle} /> Create Routine
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faUserCircle} /> My Profile
-            </li>
+            <Link
+              to="/home"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <li className="Nav-collapsed-links">
+                <span>
+                  <FontAwesomeIcon icon={faHome} color="#FFFFFF" />
+                </span>
+                Home
+              </li>
+            </Link>
+            <Link
+              to="/createRoutine"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <li className="Nav-collapsed-links">
+                <span>
+                  <FontAwesomeIcon icon={faPlusCircle} color="#FFFFFF" />
+                </span>
+                Routine
+              </li>
+            </Link>
+            <Link
+              to="/chat"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <li className="Nav-collapsed-links">
+                <span>
+                  <FontAwesomeIcon icon={faCommentDots} color="#FFFFFF" />
+                </span>
+                Chat
+              </li>
+            </Link>
+            <Link
+              to="/settings"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <li className="Nav-collapsed-links">
+                <span>
+                  <FontAwesomeIcon icon={faCog} color="#FFFFFF" />
+                </span>
+                <span className="Link">Settings</span>
+              </li>
+            </Link>
+            <Link
+              to="/"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <li className="Nav-collapsed-links" onClick={this.handleLogout}>
+                <span>
+                  <FontAwesomeIcon icon={faSignOutAlt} color="#FFFFFF" />
+                </span>
+                Log Out
+              </li>
+            </Link>
             <li>
               <hr />
             </li>
-            <li>
-              <FontAwesomeIcon icon={faCog} /> User Settings
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faSignOutAlt} /> Log Out
-            </li>
+            <li className="Header-message">Hello, {firstName}</li>
+            <Link
+              to={`/profile/user/${userId}`}
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <li className="Nav-collapsed-links">
+                <span>
+                  <FontAwesomeIcon icon={faUserCircle} color="#FFFFFF" />
+                </span>
+                Profile
+              </li>
+            </Link>
           </ul>
-        </nav> */}
+        </nav>
       </div>
     );
   }
